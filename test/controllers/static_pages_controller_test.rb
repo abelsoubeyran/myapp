ab741910268be9196239fd3dfe77e3f4ebaf3283
abelsoubeyran <abel.soubeyran@gmail.com>
2015-07-28 17:25:34 +0100

    fin static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,17 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-  end
+    assert_select "title", "Home | RoR"
+ end
 
   test "should get help" do
     get :help
     assert_response :success
+     assert_select "title", "Help | RoR"
   end
-
+  test "should get about" do
+    get :about
+    assert_response :success
+     assert_select "title", "About | RoR"  
+end
 end
